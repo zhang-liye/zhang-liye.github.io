7894e7b2a82d891fedab795c7cc5ba9fca9d045b
LeonJoness <liyezhangliye@gmail.com>
2016-07-07 11:57:32 +0800

    Site updated: 2016-07-07 11:57:31

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/expect
-eval spawn [lrange $argv 1 end]
-expect "password:"
+eval spawn [lrange $argv 2 end]
+expect "user:"
 send [lindex $argv 0]
+expect "password:"
+send [lindex $argv 1]
 send '\r'
 interact
